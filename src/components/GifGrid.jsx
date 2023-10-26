8387e9057e5b16b2c53d582be87c63298b4175f6
Nazly Zambrano <nazmi.016@gmail.com>
2023-10-26 16:30:49 -0500

    learning to use ternary conditions to hidden things

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,32 +1,23 @@
 import { useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifs";
 import { GifItem } from "./GifItem";
+import { UseFetchGifs } from "../hooks/useFetchGifs";
 
 export const GifGrid = ({ category }) => {
-  const [images, setImages] = useState([]);
-
-  const getImages = async () => {
-    const newImages = await getGifs(category);
-    setImages(newImages);
-  };
-
-  useEffect(() => {
-    getImages();
-  }, []);
+  const { images, isLoading } = UseFetchGifs(category);
 
   return (
     <>
       <h4>{category}</h4>
+      {isLoading && <h2>Cargando...</h2>}
       <div className="card-grid ">
-        <ol>
-          {images.map((image) => (
-            <GifItem
-              key={image.id}
-              // Permite desestructurar sin necesidad de repetir "image.prop" por cada propiedad que se necesite
-              {...image}
-            />
-          ))}
-        </ol>
+        {images.map((image) => (
+          <GifItem
+            key={image.id}
+            // Permite desestructurar sin necesidad de repetir "image.prop" por cada propiedad que se necesite
+            {...image}
+          />
+        ))}
       </div>
     </>
   );
--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,27 +1,8 @@
-import { useEffect, useState } from "react";
-import { getGifs } from "../helpers/getGifs";
-
-export const GifItem = ({ category }) => {
-  const [images, setImages] = useState([]);
-
-  const getImages = async () => {
-    const newImages = await getGifs(category);
-    setImages(newImages);
-  };
-
-  useEffect(() => {
-    getImages();
-  }, []);
-
+export const GifItem = ({ title, url }) => {
   return (
-    <>
-      <h4>{category}</h4>
-
-      <ol>
-        {images.map(({ id, title }) => (
-          <li key={id}>{title}</li>
-        ))}
-      </ol>
-    </>
+    <div className="card">
+      <img src={url} alt={title} />
+      <p>{title}</p>
+    </div>
   );
 };
